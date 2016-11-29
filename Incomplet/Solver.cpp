598e7630cddf9653ed32fb4cfece64a98a21e01d
smbct <buchet.s@laposte.net>
2016-11-29 22:06:11 +0100

    commentaires

--- Incomplet/Solver.cpp
+++ Incomplet/Solver.cpp
@@ -27,10 +27,12 @@ void Solver::solve() {
     Grille grille(_taille);
     int nbRestart = 0;
 
+    /* tant que la solution n'a pas été trouvée et que le nombre de restart n'est pas trop important, une recherche locale est effectuée */
     do {
 
         rechercheLocale(grille);
 
+        /* si à l'issue de la recherche locale une solution n'a pas été trouvée, un restart est effectué */
         if(grille.score() > 0) {
             grille.restart();
             nbRestart ++;
@@ -39,6 +41,7 @@ void Solver::solve() {
 
     } while(grille.score() != 0 && nbRestart < 500);
 
+    /* si la solution n'a toujours pas été trouvée, une recherche tabou est exécutée */
     if(grille.score() > 0) {
         cout << "La recherche locale n'a pas encore trouvée de solution, lancement de la recherche tabou." << endl;
         recherchTabou(grille);
@@ -58,25 +61,33 @@ void Solver::rechercheLocale(Grille& grille) {
     bool stop = false;
     int scoreMin = grille.score();
 
+    /* tant qu'un voisin améliore le score, la recherche continue */
     while(!stop) {
 
         int eltaMin = -1, eltbMin = -1;
         int scoreVoisin = -1;
 
+        /* calcul chaque permutation de deux éléments */
         for(int i = 0; i < _taille*_taille; i++) {
             for(int j = i+1; j < _taille*_taille; j++) {
 
+                /* effectue la permutation */
                 grille.echange(i, j);
 
-                if(scoreVoisin == -1 || grille.score() < scoreVoisin) {
+                /* le meilleur score des voisins est retenu */
+                if(scoreVoisin == -1 || static_cast<int>(grille.score()) < scoreVoisin) {
                     scoreVoisin = grille.score();
+                    /* les élements permutés sont retenus pour retrouver la permutation */
                     eltaMin = i;
                     eltbMin = j;
                 }
 
-                grille.echange(j, i); /* annule l'échange */
+                /* l'échange est annulé pour procéder aux autres échanges */
+                grille.echange(j, i);
             }
         }
+
+        /* si un voisin avait un meilleur score, le score est mis à jour et la recherche continue, sinon la recherche s'arrête */
         if(scoreVoisin < scoreMin) {
             scoreMin = scoreVoisin;
             grille.echange(eltaMin, eltbMin);
@@ -99,8 +110,6 @@ void Solver::solverRecuit() {
     int L = 50; /* nombre d'itération pour une température constante */
     double alpha = 0.84; /* coefficient de refroidissement */
 
-    int nbItMax = 100;
-
     int it = 1;
     bool stop = false;
 
@@ -114,6 +123,8 @@ void Solver::solverRecuit() {
 
         grille.echangeAlea(); /* voisin aléatoire */
         delta += grille.score();
+
+        /* calcul de la proba d'acceptation du voisin */
 
         bool accepte = false;
         if(delta < 0) {
@@ -139,6 +150,7 @@ void Solver::solverRecuit() {
 
                 // cout << "score : " << scoreMin << endl;
 
+                /* si le score est assez petit, l'algorithme termine par une recherche tabou, pour grater vers la solution */
                 if(grille.score() <= 10) {
                     // cout << "tabuMode" << endl << endl;
                     recherchTabou(grille);
@@ -153,17 +165,19 @@ void Solver::solverRecuit() {
 
         it ++;
 
-        if(it % L == 0) { // update temperature
+        /* la température est mise à jour */
+        if(it % L == 0) {
             T *= alpha;
         }
 
+        /* si la température est trop basse, un réchauffement est effectué */
         if(T < 1e-20) {
             T = T0;
             if(T0 > 1) {
                 T0 *= 0.4;
             }
             if(L < 1e12) {
-                L *= 1.5;
+                L = static_cast<int> (static_cast<double>(L) * 1.5);
             }
         }
 
@@ -181,22 +195,25 @@ void Solver::solverRecuit() {
 /*----------------------------------------------------------------------------*/
 void Solver::recherchTabou(Grille& grille) {
 
+    /* longueur tabou classique */
     const int longueurTabou = 7;
-    vector<vector<int>> listeTabou(_taille*_taille);
+
+    /* initialisation de la liste tabou */
+    vector<vector<unsigned int>> listeTabou(_taille*_taille);
     for(int i = 0; i < _taille*_taille; i++) {
         listeTabou[i].resize(_taille*_taille, 1);
     }
 
 
     bool stop = false;
-    int nbIt = 1;
+    unsigned int nbIt = 1;
 
-    int meilleurScore = grille.score();
+    unsigned int meilleurScore = grille.score();
 
     while(!stop) {
 
         int eltaMin, eltbMin;
-        unsigned int scoreVoisin = -1;
+        int scoreVoisin = -1;
 
         /* recherche locale : exploration de tous les voisins */
         for(int i = 0; i < _taille*_taille; i++) {
@@ -205,7 +222,7 @@ void Solver::recherchTabou(Grille& grille) {
                 /* si l'échange n'est pas tabou, on peut l'essayer */
                 if(listeTabou[i][j] <= nbIt || grille.score() < meilleurScore) {
                     grille.echange(i, j);
-                    if(scoreVoisin == -1 || grille.score() < scoreVoisin) {
+                    if(scoreVoisin == -1 || static_cast<int>(grille.score()) < scoreVoisin) {
                         scoreVoisin = grille.score();
                         eltaMin = i;
                         eltbMin = j;
@@ -218,9 +235,11 @@ void Solver::recherchTabou(Grille& grille) {
 
         if(scoreVoisin != -1) {
 
+            /* on bascule vers la configuration du voisin non tabou ayant le meilleur score */
             grille.echange(eltaMin, eltbMin);
             listeTabou[eltaMin][eltbMin] = nbIt + longueurTabou;
 
+            /* si le score est de 0, la recherche s'arrête */
             if(grille.score() == 0) {
                 stop = true;
             }
@@ -229,9 +248,8 @@ void Solver::recherchTabou(Grille& grille) {
                 meilleurScore = grille.score();
             }
 
-            // cout << "tabu score : " << grid.score() << endl;
-
         } else {
+            /* si tous les éléments sont tabous, il n'y a plus de voisin à explorer, la recherche s'arrête */
             stop = true;
         }
 
